99a47191501452516dcdd7fbe9a4667618f4887c
tanlooger <tanlooger@gmail.com>
2021-12-05 12:26:16 +0800

    1

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,7 @@
 import type { NextFetchEvent, NextRequest } from 'next/server'
 
 export function middleware (req: NextRequest, ev: NextFetchEvent) {
-  console.log(req)
+  console.log('ip地址 ' + req.ip)
+  console.log(req.headers.get('user-agent'))
   // return new Response('adsf')
 }
