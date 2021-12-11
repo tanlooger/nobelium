11612273346419dae66a87894f8cd083a35b2c1c
tanlooger <tanlooger@gmail.com>
2021-12-12 07:12:26 +0800

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,8 @@ const BLOG = require('./blog.config')
 module.exports = {
   siteUrl: BLOG.link,
   generateRobotsTxt: true,
-  sitemapSize: 7000
+  sitemapSize: 7000,
+  exclude: ['/feed', '/search', '/books/*', '/page/*', '/0/*']
   // ...other options
   // https://github.com/iamvishnusankar/next-sitemap#configuration-options
 }
